ac1e1cb1b20afdbbbfb25dfc932dd95a9e0a3d74
ESONHUGH <32677240+Esonhugh@users.noreply.github.com>
2022-04-25 11:29:56 +0800

    unfinished Login and Register

--- src/components/Auth/Login.svelte
+++ src/components/Auth/Login.svelte
@@ -1,15 +1,36 @@
 <script>
     import Unfinished from "../Unfinished.svelte";
+
+    import Button,{Label} from '@smui/button';
+    import Textfield from '@smui/textfield';
+    import HelperText from '@smui/textfield/helper-text';
+    let userLogin = {
+        username: "",
+        password: ""
+    }
+    async function Login(){
+        alert("Submitted")
+    }
 </script>
 
-<p> This is Login </p>
 <Unfinished/>
-
-
-
+<div>
+    <Textfield variant="outlined" bind:value={userLogin.username} label="Username">
+        <HelperText slot="helper">Username</HelperText>
+    </Textfield>
+    <Textfield variant="outlined"  bind:value={userLogin.password} label="Password">
+        <HelperText slot="helper">Password</HelperText>
+    </Textfield>
+</div>
+<div>
+    <Button on:click={Login} variant="raised">
+        <Label>Submit</Label>
+    </Button>
+</div>
 
 <style>
-    p {
-        text-align: center;
+    div {
+        margin: auto;
+        padding: 2px;
     }
 </style>
--- src/components/Auth/Register.svelte
+++ src/components/Auth/Register.svelte
@@ -1,10 +1,41 @@
 <script>
     import Unfinished from "../Unfinished.svelte";
+
+    import Button,{Label} from '@smui/button';
+    import Textfield from '@smui/textfield';
+    import HelperText from '@smui/textfield/helper-text';
+    let userLogin = {
+        username: "",
+        password: "",
+        email: ""
+    }
+    async function Login(){
+        alert("Submitted")
+    }
 </script>
-<p> This is Register! Working</p>
+
 <Unfinished/>
+
+<div>
+    <Textfield variant="outlined" bind:value={userLogin.username} label="Username">
+        <HelperText slot="helper">Username</HelperText>
+    </Textfield>
+    <Textfield variant="outlined"  bind:value={userLogin.password} label="Password">
+        <HelperText slot="helper">Password</HelperText>
+    </Textfield>
+    <Textfield variant="outlined"  bind:value={userLogin.email} label="Email">
+        <HelperText slot="helper">Email</HelperText>
+    </Textfield>
+</div>
+<div>
+    <Button on:click={Login} variant="raised">
+        <Label>Submit</Label>
+    </Button>
+</div>
+
 <style>
-    p {
-        text-align: center;
+    div {
+        margin: auto;
+        padding: 2px;
     }
 </style>
