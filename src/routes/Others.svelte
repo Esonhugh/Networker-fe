4baafcebc60c0463f4b22a1f15b4721c5abd9349
ESONHUGH <32677240+Esonhugh@users.noreply.github.com>
2022-04-29 18:32:00 +0800

    add login error banner in Others.

--- src/routes/Others.svelte
+++ src/routes/Others.svelte
@@ -1,11 +1,15 @@
 <script>
-    export const name = "Others";
+    import {GetJWTObj} from "../components/token";
+    import Alert from "../components/AlertRedirect.svelte";
     import SimpleConfigList from "../components/ConfigTable/SimpleConfigList.svelte";
     import Card from '@smui/card';
     import {apiBase} from "../setting";
+
+    export const name = "Others";
     let fetchUrl = $apiBase + "/peerinfo/list"
+
     async function getFriendsConfig() {
-        const res = await fetch(fetchUrl,{
+        const res = await fetch(fetchUrl, {
             method: "GET",
             mode: "cors",
             headers: {
@@ -13,25 +17,33 @@
             },
         });
         const data = await res.json()
-        if (res.ok){
+        if (res.ok) {
             return data
-        }else {
+        } else {
             throw data
         }
+    }
+
+    function verifyLogin() {
+        return GetJWTObj() !== null;
     }
 </script>
 
 <div>
-    <Card>
-        <h1>Config List</h1>
-        {#await getFriendsConfig()}
-            <pre>Loading....</pre>
-        {:then json}
-            <SimpleConfigList configList="{json}"/>
-        {:catch err}
-            <pre>{err.errormsg}</pre>
-        {/await}
-    </Card>
+    {#if (verifyLogin())}
+        <Card>
+            <h1>Config List</h1>
+            {#await getFriendsConfig()}
+                <pre>Loading....</pre>
+            {:then json}
+                <SimpleConfigList configList="{json}"/>
+            {:catch err}
+                <pre>{err.errormsg}</pre>
+            {/await}
+        </Card>
+    {:else}
+        <Alert open message="You are not login" url="#/auth/"/>
+    {/if}
 </div>
 
 <style>
